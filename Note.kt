2e02994a4fe23cf24fe9574274cb84daaebb2cdd
lalailac <preciousballaso2@gmail.com>
2025-06-12 20:38:54 +0800

    Delete Note.kt

--- Note.kt
+++
@@ -1,6 +0,0 @@
-@Entity(tableName = "note_table")
-data class Note(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val content: String,
-    val timestamp: Long = System.currentTimeMillis()
-)
